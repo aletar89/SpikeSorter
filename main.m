3d35700ce63369b6e12a172b4a811149ab10cec1
aletar_lab_comp <aletar89@gmail.com>
2019-07-17 16:24:29 +0300

    data loading and verbosity

--- main.m
+++ main.m
@@ -1,4 +1,15 @@
 total_timer = tic;
+addpath('source')
+disp("Start")
+tic
+raw_data = load('data/mC41_33/sh1_30.mat'); raw_data = raw_data.sh1_30;
+true_spike_times = load('data/mC41_33/res1_30.mat'); true_spike_times = true_spike_times.res1_30;
+true_clusters = load('data/mC41_33/clu1_30.mat'); true_clusters = true_clusters.clu1_30;
+
+last_data_ind = round(size(raw_data,2)/10);
+raw_data = raw_data(:,1:last_data_ind);
+true_spike_times = true_spike_times(true_spike_times < last_data_ind);
+true_clusters = true_clusters(1:length(true_spike_times));
 
 preprocess = @(raw_data) bandpass(double(raw_data'), [250, 6000], 20000)';
 detection = @(clean_data) my_first_detector(clean_data,4.5,1.05,100);
@@ -6,33 +17,31 @@ feature_extraction = @(clean_data,spike_times) spike_energy(clean_data,spike_tim
 clustering = @(features) kmeans(squeeze(features)',15,'Distance', 'cityblock', 'Replicates',5, 'MaxIter',1000);
 
 S = SpikeSorter(preprocess, detection, feature_extraction, clustering);
+disp("Finished loading data"), toc
+
 tic
-clean_data = S.pre_process(sh1_30(:,:));
-disp("Finished cleaning data")
-toc
+clean_data = S.pre_process(raw_data);
+disp("Finished cleaning data"), toc
+
 tic
 spike_times = S.detection(clean_data);
-disp("Finished detecting_spikes")
-toc
+disp("Finished detecting_spikes"), toc
+
 tic
-true_spike_times = res1_30(res1_30 < size(clean_data,2));
 margin = 10;
 detection_rating = S.test_detection(spike_times, true_spike_times, margin);
-disp(sprintf("Detection rating is %.1f%%", detection_rating*100))
-toc
+disp(sprintf("Detection rating is %.1f%%", detection_rating*100)), toc
+
 tic
 features = S.feature_extraction(clean_data, spike_times);
-disp("Finished extracting features")
-toc
+disp("Finished extracting features"), toc
+
 tic
 clusters = S.clustering(features);
-disp("Finished clustering")
-toc
-tic
-clustering_rating = S.test_clustering(spike_times, clusters, true_spike_times, clu1_30(1:length(true_spike_times)), margin);
-clustering_rating = S.test_clustering(spike_times, clusters, spike_times, clusters2, margin);
+disp("Finished clustering"), toc
 
-disp(sprintf("Clustering rating is %.1f%%", clustering_rating*100))
-toc
-disp("Total time:")
-toc(total_timer)
+tic
+clustering_rating = S.test_clustering(spike_times, clusters, true_spike_times, true_clusters, margin);
+disp(sprintf("Clustering rating is %.1f%%", clustering_rating*100)), toc
+
+disp(sprintf("Total time: %.2f seconds", toc(total_timer)))
