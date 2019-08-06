5d6a989f60d8f472e94ddaaf25d5ce74fe7b4291
aletar_lab_comp <aletar89@gmail.com>
2019-08-06 09:25:24 +0300

    partial display of clusters

--- display_clustering.m
+++ display_clustering.m
@@ -13,22 +13,33 @@ spike_forms = zeros(size(data,1), 2*half_width + 1, length(spike_times));
 % % % commented out detrending, uncoment lines 7-15 to use detrended data
 %slope = repmat(linspace(0,1,33),size(data,1),1);
 for i=1:length(spike_times)
-%     first_val = mean(data(:,spike_times(i)-half_width:spike_times(i)-half_width+2),2);
-%     first_val = repmat(first_val, 1, 2*half_width+1);
-%     lsat_val = mean(data(:,spike_times(i)+half_width-2:spike_times(i)+half_width),2);
-%     last_val = repmat(lsat_val, 1, 2*half_width+1);
-%     detrend = first_val - slope.*last_val;
+    %     first_val = mean(data(:,spike_times(i)-half_width:spike_times(i)-half_width+2),2);
+    %     first_val = repmat(first_val, 1, 2*half_width+1);
+    %     lsat_val = mean(data(:,spike_times(i)+half_width-2:spike_times(i)+half_width),2);
+    %     last_val = repmat(lsat_val, 1, 2*half_width+1);
+    %     detrend = first_val - slope.*last_val;
     spike_forms(:,:,i) = data(:,spike_times(i)-half_width:spike_times(i)+half_width);
-%     spike_forms(:,:,i) = spike_forms(:,:,i) - detrend;
+    %     spike_forms(:,:,i) = spike_forms(:,:,i) - detrend;
 end
 
 colors = [1,0,0;
-          0,1,0;
-          0,0,1;
-          0.5,0.5,0;
-          0.5,0,0.5;
-          0,0.5,0.5];
+    0,1,0;
+    0,0,1;
+    0.5,0.5,0;
+    0.5,0,0.5;
+    0,0.5,0.5];
 cluster_ids = unique(clusters);
+while length(cluster_ids)>6
+    spikes_per_cluster = sum(clusters == cluster_ids');
+    [~,count_sort] = sort(spikes_per_cluster);
+    least_spikes = cluster_ids(count_sort(1));
+    spike_times(clusters==least_spikes) = [];
+    if ~isempty(features)
+        features(:,clusters==least_spikes) = [];
+    end
+    clusters(clusters==least_spikes) = [];
+    cluster_ids = unique(clusters);
+end
 chained_spikes = reshape(permute(spike_forms,[2,1,3]),[size(spike_forms,1)*size(spike_forms,2), size(spike_forms,3)])';
 if isempty(features)
     coef = pca(chained_spikes);
@@ -38,36 +49,62 @@ else
     pca_space = features'*coef;
 end
 
-%% 3D PCA scatter
+%% PCA scatters
 figure
+max_points = 1000;
 for i = 1:length(cluster_ids)
     c = cluster_ids(i);
+    points = find(clusters==c);
+    if length(points)>max_points
+        selected = round(length(points)*rand(1,max_points));
+        while min(selected)==0
+            selected = round(length(points)*rand(1,max_points));
+        end
+        points = points(selected);
+    end
     subplot(2,2,1)
-    scatter(pca_space(clusters==c,1), pca_space(clusters==c,2),[],colors(i,:),'filled','markerfacealpha',0.1)
+    scatter(pca_space(points,1), pca_space(points,2),[],colors(i,:),'filled','markerfacealpha',0.1)
+    xlabel('PC1');
+    ylabel('PC2');
     hold on
     subplot(2,2,2)
-    scatter(pca_space(clusters==c,1), pca_space(clusters==c,3),[],colors(i,:),'filled','markerfacealpha',0.1)
+    scatter(pca_space(points,1), pca_space(points,3),[],colors(i,:),'filled','markerfacealpha',0.1)
+    xlabel('PC1');
+    ylabel('PC3');
     hold on
     subplot(2,2,3)
-    scatter(pca_space(clusters==c,1), pca_space(clusters==c,4),[],colors(i,:),'filled','markerfacealpha',0.1)
+    scatter(pca_space(points,1), pca_space(points,4),[],colors(i,:),'filled','markerfacealpha',0.1)
+    xlabel('PC1');
+    ylabel('PC4');
     hold on
     subplot(2,2,4)
-    scatter(pca_space(clusters==c,1), pca_space(clusters==c,5),[],colors(i,:),'filled','markerfacealpha',0.1)
+    scatter(pca_space(points,1), pca_space(points,5),[],colors(i,:),'filled','markerfacealpha',0.1)
+    xlabel('PC1');
+    ylabel('PC5');
     hold on
 end
 legend( num2str(unique(clusters)))
 
 %% Spike shapes
 figure
+max_points = 500;
 for i = 1:length(cluster_ids)
     c = cluster_ids(i);
-    alpha = max(min(15/sum(clusters==c),1),0.0025);
+    points = find(clusters==c);
+    if length(points)>max_points
+        selected = round(length(points)*rand(1,max_points));
+        while min(selected)==0
+            selected = round(length(points)*rand(1,max_points));
+        end
+        points = points(selected);
+    end
+    alpha = max(min(15/length(points),1),0.03);
     ax1 = subplot(1,5,1:3);
-    plot(chained_spikes(clusters==c,:)','color',[colors(i,:), alpha])
+    plot(chained_spikes(points,:)','color',[colors(i,:), alpha])
     hold on
-    plot(mean(chained_spikes(clusters==c,:)),'color',colors(i,:),'linewidth',3);
+    plot(mean(chained_spikes(points,:)),'color',colors(i,:),'linewidth',3);
     ax2 = subplot(1,5,4:5);
-    plot(chained_spikes(clusters==c,:)'-i*2500,'color',[colors(i,:), alpha])
+    plot(chained_spikes(points,:)'-i*2500,'color',[colors(i,:), alpha])
     hold on
 end
 linkaxes([ax1, ax2],'x')
